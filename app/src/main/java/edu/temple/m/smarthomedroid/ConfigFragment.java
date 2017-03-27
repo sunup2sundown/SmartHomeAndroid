60015eefa8bcdf5392baedf8c21d91e8f864e1be
kunkun5294 <kunkun5294@yahoo.com>
2017-03-27 00:45:47 -0400

    Fragment Switching fix

--- app/src/main/java/edu/temple/m/smarthomedroid/ConfigFragment.java
+++ app/src/main/java/edu/temple/m/smarthomedroid/ConfigFragment.java
@@ -1,10 +1,20 @@
 package edu.temple.m.smarthomedroid;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by M on 3/16/2017.
  */
 
 public class ConfigFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View v = inflater.inflate(R.layout.thirdlayout,container,false);
+        return v;
+    }
 }
