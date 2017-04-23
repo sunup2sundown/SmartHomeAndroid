7bdfd395e206689477928d981efa19d35d3c68f9
M <mjdev3030@gmail.com>
2017-04-23 16:34:17 -0400

    Camera Feed Fragment, Camera Tab populated and Additional Voice Command Functionality

---
+++ app/src/main/java/edu/temple/m/smarthomedroid/Adapters/CameraAdapter.java
@@ -0,0 +1,8 @@
+package edu.temple.m.smarthomedroid.Adapters;
+
+/**
+ * Created by M on 4/23/2017.
+ */
+
+public class CameraAdapter {
+}
---
+++ app/src/main/java/edu/temple/m/smarthomedroid/CameraFeedFragment.java
@@ -0,0 +1,8 @@
+package edu.temple.m.smarthomedroid;
+
+/**
+ * Created by M on 4/23/2017.
+ */
+
+public class CameraFeedFragment {
+}
